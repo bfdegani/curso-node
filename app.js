a22dc2f75a862e2a28fa6dce65fed6ddef470c46
bfdegani <bruno.degani@gmail.com>
2017-09-06 20:45:16 -0300

    usando websocket com socket.io para notificar o client

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var app = require('./config/express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http); // socket.io recebe o objeto http.Server criado a partir do express
+
+app.set('io', io); //insere o objeto io criado dentro do objeto app
 
 hostname = "127.0.0.1";
 port = "3000";
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
---
+++ app/routes/promocoes.js
@@ -0,0 +1,20 @@
+module.exports = function(app) {
+
+  app.get('/promocoes/form',function(req,res){
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection); //new cria um novo contexto para esse objeto, do contrario, 'this' referencia todo o contexto do express
+
+    produtosDAO.lista(function(erros, resultados){
+      //console.log(resultados);
+      res.render('promocoes/form',{lista:resultados}); //binding dos resultados com o elemento da página
+    });
+    connection.end();
+  });
+
+  app.post('/promocoes',function(req,res){
+    var promocao = req.body;
+    console.log(promocao);
+    app.get('io').emit('novaPromocao', promocao);
+    res.redirect('/promocoes/form');
+  });
+}

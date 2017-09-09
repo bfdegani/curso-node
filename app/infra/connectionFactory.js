4c7e6abfb5b8b348cf5bb19333b6f6f7243e363e
bfdegani <bruno.degani@gmail.com>
2017-09-09 02:43:14 -0300

    inicio implementacao pool'

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -11,7 +11,7 @@ function createDBConnection(){
       });
   }
 
-  if(process.env.NODE_ENV === 'test') { // '===' difere de '==' por nao fazer conversao de tipo e só retorna true se os objetos são do mesmo tipo e iguais (nesse caso, não faz diferença)
+  else if(process.env.NODE_ENV === 'test') { // '===' difere de '==' por nao fazer conversao de tipo e só retorna true se os objetos são do mesmo tipo e iguais (nesse caso, não faz diferença)
     console.log('running in test database');
     return mysql.createConnection({
       host : 'localhost',
@@ -21,12 +21,10 @@ function createDBConnection(){
     });
   }
 
-  if(process.env.NODE_ENV === 'production') {
+  else if(process.env.NODE_ENV === 'production') {
     console.log('running in remote production database');
     return mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-}
-
-
+  }
 }
 
 //wrapper
